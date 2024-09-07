5eec7ed4ec24de2d4b9a814ba05a361bd2490ce8
biniyam nahusenay <bininahu12@gmail.com>
2024-09-07 09:46:24 +0300

    unUsed declaration removed

--- frontend/src/pages/SongStatistics.tsx
+++ frontend/src/pages/SongStatistics.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 import { fetchStatisticsStart } from '../redux/slices/statSlice';
