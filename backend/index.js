4eabdece90f2330d7729f88844ab13bf853c620f
biniyam nahusenay <bininahu12@gmail.com>
2024-09-07 08:42:22 +0300

    add preInstallation

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors'
+import path from 'path'
 
 import connectDB from "./db/db.js";
 import songRoute from "./routes/songRoute.js"
 
 dotenv.config();
 connectDB();
+
+const __dirname = path.resolve()
 
 const app = express();
 
@@ -23,5 +26,11 @@ app.get("/", (req, res) => {
   });
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'/frontend/dist/index.html'))
+})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
