87e2c25dcb7b4d5d906a0ac8017867d5aa99b541
biniyam nahusenay <bininahu12@gmail.com>
2024-09-11 08:54:07 +0300

    updatin the preinstallation

--- backend/index.js
+++ backend/index.js
@@ -25,13 +25,13 @@ app.get("/", (req, res) => {
     res.status(201).json({ message: "Server is  up and running" });
   });
 
-  //pass = h6lsShZe2aOTEyTl and username = bininahu12
+ 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname,'/frontend/dist')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'/frontend/dist/index.html'))
+    res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
